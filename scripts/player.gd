283bf35c80f823243c1f5680c3850b38cd1bb5bc
Frenky Bojler <polak.tomas01@gmail.com>
2026-03-27 19:40:40 +0100

    capture mous on web

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,7 +19,9 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 # Handle player movement
